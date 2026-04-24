8addcaeea1bd649fa8d202b140b61b83490dd417
nithinK-142 <nithin.sagar.359@gmail.com>
2026-04-24 17:38:52 +0530

    update: db connection info

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,18 @@ import { logWithTimestamp } from "../utils/logger.js";
 
 export const connectDB = async () => {
   try {
+    const start = Date.now();
     await mongoose.connect("mongodb://127.0.0.1:27017/animeDB");
-    logWithTimestamp("✅ MongoDB Connected");
+    const conn = mongoose.connection;
+    const time = Date.now() - start;
+    console.log(
+      `✅ MongoDB Connected!
+• Host: ${conn.host}
+• PORT: ${conn.port}
+• Database: ${conn.name}
+• Ready State: ${conn.readyState} (1 = connected)
+• Connection Time: ${time}ms`
+    );
   } catch (err) {
     logWithTimestamp("❌ MongoDB Error", err.message);
     process.exit(1);
